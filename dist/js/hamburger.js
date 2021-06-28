7d87102ef3d7d51f0ef2dbc37ae1f9b9de7a758f
jesse10930 <ronda.jesse@gmail.com>
2021-06-28 12:25:12 -0400

    fixing responsiveness issues

--- dist/js/hamburger.js
+++ dist/js/hamburger.js
@@ -6,22 +6,49 @@ const docBody = document.querySelector('body');
 const docHtml = document.querySelector('html');
 const overlay = document.getElementById('overlay');
 
-// Hamburger Click
-hamburgerImg.onclick = (e) => {
+// Show dropdown nav for mobile
+const showDropdown = () => {
   hamburgerImg.style.display = 'none';
   closeImg.style.display = 'block';
   dropdownMenu.style.display = 'flex';
+  overlay.style.display = 'block';
   docBody.style.overflow = 'hidden';
   docHtml.style.overflow = 'hidden';
-  overlay.style.display = 'block';
 };
 
-// Close Click
-closeImg.onclick = (e) => {
-  closeImg.style.display = 'none';
+// Hide dropdown nav for mobile
+const hideDropdown = () => {
   hamburgerImg.style.display = 'block';
+  closeImg.style.display = 'none';
   dropdownMenu.style.display = 'none';
+  overlay.style.display = 'none';
   docBody.style.overflow = 'visible';
   docHtml.style.overflow = 'visible';
-  overlay.style.display = 'none';
 };
+
+// Close Mobile Header when Screen Size Grows
+const closeMobileHeader = () => {
+  if (window.innerWidth > 500) {
+    hamburgerImg.style.display = 'none';
+    closeImg.style.display = 'none';
+    dropdownMenu.style.display = 'none';
+    overlay.style.display = 'none';
+    docBody.style.overflow = 'visible';
+    docHtml.style.overflow = 'visible';
+  } else {
+    if (
+      hamburgerImg.style.display === 'block' ||
+      (hamburgerImg.style.display === 'none' &&
+        closeImg.style.display === 'none')
+    ) {
+      hideDropdown();
+    } else {
+      showDropdown();
+    }
+  }
+};
+
+// Events
+window.addEventListener('resize', closeMobileHeader);
+hamburgerImg.onclick = () => showDropdown();
+closeImg.onclick = () => hideDropdown();
