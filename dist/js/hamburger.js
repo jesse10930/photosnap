408ae48a433b7f19c0ed52a18fc081f359c3783f
jesse10930 <ronda.jesse@gmail.com>
2021-06-29 10:30:09 -0400

    refactoring code

--- dist/js/hamburger.js
+++ dist/js/hamburger.js
@@ -1,54 +1,56 @@
-// Declare variables
-const hamburgerImg = document.getElementById('hamburger-img');
-const closeImg = document.getElementById('close-img');
-const dropdownMenu = document.getElementById('dropdown-menu');
-const docBody = document.querySelector('body');
-const docHtml = document.querySelector('html');
-const overlay = document.getElementById('overlay');
+(function () {
+  // Declare variables
+  const hamburgerImg = document.getElementById('hamburger-img');
+  const closeImg = document.getElementById('close-img');
+  const dropdownMenu = document.getElementById('dropdown-menu');
+  const docBody = document.querySelector('body');
+  const docHtml = document.querySelector('html');
+  const overlay = document.getElementById('overlay');
 
-// Show dropdown nav for mobile
-const showDropdown = () => {
-  hamburgerImg.style.display = 'none';
-  closeImg.style.display = 'block';
-  dropdownMenu.style.display = 'flex';
-  overlay.style.display = 'block';
-  docBody.style.overflow = 'hidden';
-  docHtml.style.overflow = 'hidden';
-};
-
-// Hide dropdown nav for mobile
-const hideDropdown = () => {
-  hamburgerImg.style.display = 'block';
-  closeImg.style.display = 'none';
-  dropdownMenu.style.display = 'none';
-  overlay.style.display = 'none';
-  docBody.style.overflow = 'visible';
-  docHtml.style.overflow = 'visible';
-};
-
-// Close Mobile Header when Screen Size Grows
-const closeMobileHeader = () => {
-  if (window.innerWidth > 500) {
+  // Show dropdown nav for mobile
+  const showDropdown = () => {
     hamburgerImg.style.display = 'none';
+    closeImg.style.display = 'block';
+    dropdownMenu.style.display = 'flex';
+    overlay.style.display = 'block';
+    docBody.style.overflow = 'hidden';
+    docHtml.style.overflow = 'hidden';
+  };
+
+  // Hide dropdown nav for mobile
+  const hideDropdown = () => {
+    hamburgerImg.style.display = 'block';
     closeImg.style.display = 'none';
     dropdownMenu.style.display = 'none';
     overlay.style.display = 'none';
     docBody.style.overflow = 'visible';
     docHtml.style.overflow = 'visible';
-  } else {
-    if (
-      hamburgerImg.style.display === 'block' ||
-      (hamburgerImg.style.display === 'none' &&
-        closeImg.style.display === 'none')
-    ) {
-      hideDropdown();
-    } else {
-      showDropdown();
-    }
-  }
-};
+  };
 
-// Events
-window.addEventListener('resize', closeMobileHeader);
-hamburgerImg.onclick = () => showDropdown();
-closeImg.onclick = () => hideDropdown();
+  // Close Mobile Header when Screen Size Grows
+  const closeMobileHeader = () => {
+    if (window.innerWidth > 500) {
+      hamburgerImg.style.display = 'none';
+      closeImg.style.display = 'none';
+      dropdownMenu.style.display = 'none';
+      overlay.style.display = 'none';
+      docBody.style.overflow = 'visible';
+      docHtml.style.overflow = 'visible';
+    } else {
+      if (
+        hamburgerImg.style.display === 'block' ||
+        (hamburgerImg.style.display === 'none' &&
+          closeImg.style.display === 'none')
+      ) {
+        hideDropdown();
+      } else {
+        showDropdown();
+      }
+    }
+  };
+
+  // Events
+  window.addEventListener('resize', closeMobileHeader);
+  hamburgerImg.onclick = () => showDropdown();
+  closeImg.onclick = () => hideDropdown();
+})();
